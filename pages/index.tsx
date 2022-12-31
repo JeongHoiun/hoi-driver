521f8dc54dcf3dcbf86dae926142d7676a5c83dd
hoiunjeong <un4255@gmail.com>
2023-01-01 00:13:32 +0900

    home component에 사이드바 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Sidebar from '../components/commons/Sidebar';
 import MainLayout from '../components/commons/MainLayout';
 
 function HomeContents() {
-    return (
-        <>
-            <Head>
-                <title>Hoi Driver</title>
-                <meta name="description" content="Hoiun's driver" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={styles.main}>
-                <Sidebar />
-            </main>
-        </>
-    );
+    return <>This is Home</>;
 }
 
 export default function Home() {
