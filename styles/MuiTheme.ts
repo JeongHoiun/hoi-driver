b50e1cdc703aad22769bad8ad648e26db97a2455
hoiunjeong <un4255@gmail.com>
2023-01-08 13:07:49 +0900

    typography 전역 스타일 추가

--- styles/MuiTheme.ts
+++ styles/MuiTheme.ts
@@ -9,6 +9,50 @@ export const MuiTheme = createTheme({
             main: '#ff8e88'
         }
     },
+    typography: {
+        subtitle1: {
+            fontSize: '14px',
+            fontWeight: 500,
+            color: '#333'
+        },
+        subtitle2: {
+            fontSize: '12px',
+            fontWeight: 500,
+            color: '#333'
+        },
+        body1: {
+            fontSize: '14px',
+            color: '#666'
+        },
+        body2: {
+            color: '#666',
+            fontSize: '12px'
+        },
+        h6: {
+            color: '#333',
+            fontSize: '14px'
+        },
+        h5: {
+            color: '#333',
+            fontSize: '16px'
+        },
+        h4: {
+            color: '#333',
+            fontSize: '18px'
+        },
+        h3: {
+            color: '#333',
+            fontSize: '20px'
+        },
+        h2: {
+            color: '#333',
+            fontSize: '24px'
+        },
+        h1: {
+            color: '#333',
+            fontSize: '32px'
+        }
+    },
     components: {
         MuiButtonBase: {
             defaultProps: {
