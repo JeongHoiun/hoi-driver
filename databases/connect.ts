d45b13765cc5739bedc44d0e868050c271ec1964
hoiunjeong <un4255@gmail.com>
2023-02-17 14:59:30 +0900

    localhost로 하면 갑자기 mysql 연결이 안되는 현상 발생.
    localhost -> 127.0.0.1로 변경

--- databases/connect.ts
+++ databases/connect.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 const mysql_connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'hoiun',
     password: '46wh6386!',
     database: 'hoi_driver'
