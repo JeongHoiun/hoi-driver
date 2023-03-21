c77b311e416a3973adba664791c0956a25582b84
hoiunjeong <un4255@gmail.com>
2023-03-21 17:27:17 +0900

    환경변수를 읽어서 aws Access

--- aws/connect.ts
+++ aws/connect.ts
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: '----------',
-    secretAccessKey: '----------------',
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY,
     region: 'ap-northeast-2',
     signatureVersion: 'v4'
 });
