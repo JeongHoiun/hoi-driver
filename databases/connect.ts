433eb288cf0f4eacb7dd51dea5b1db37846ce0a3
hujeong <hujeong@suresofttech.com>
2023-12-13 09:21:43 +0900

    database 정보 삭제

--- databases/connect.ts
+++ databases/connect.ts
@@ -2,9 +2,9 @@ import mysql from 'mysql';
 
 const mysql_connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'hoiun',
-    password: '46wh6386!',
-    database: 'hoi_driver'
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 export default mysql_connection;
