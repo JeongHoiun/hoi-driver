fd5ac321c688172ed4028e3ad3020bc2da550ba5
hoiunjeong <un4255@gmail.com>
2023-12-18 20:38:04 +0900

    전역 snackbar 패키지 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SnackbarProvider } from 'notistack';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MuiTheme } from '../styles/MuiTheme';
 import '../aws/connect';
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <ThemeProvider theme={MuiTheme}>
-                <Component {...pageProps} />
+                <SnackbarProvider maxSnack={3}>
+                    <Component {...pageProps} />
+                </SnackbarProvider>
             </ThemeProvider>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
