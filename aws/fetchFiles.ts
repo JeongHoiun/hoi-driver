cde5f0bfea2810ffcdeaab63104082d9d2e97459
hoiunjeong <un4255@gmail.com>
2023-12-24 16:01:27 +0900

    데이터베이스에서 10개씩 끊어서 가져오므로 삭제

--- aws/fetchFiles.ts
+++ aws/fetchFiles.ts
@@ -3,8 +3,7 @@ import AWS from 'aws-sdk';
 export async function fetchSpecificPageItems(keys: string[]) {
     try {
         const s3 = new AWS.S3();
-        // TODO: 10개씩 끊어서 가져오기
-        const promises = keys.splice(0, 9).map((v) =>
+        const promises = keys.map((v) =>
             s3.getObject({ Bucket: 'hoi-driver', Key: v as string }).promise()
         );
 
