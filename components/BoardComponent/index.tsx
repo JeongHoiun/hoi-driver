ebbd6a2904b51d9db0a9c4f7e7cf70dc711b2e63
hoiunjeong <un4255@gmail.com>
2024-01-02 20:57:04 +0900

    paging에 대해 query param 추가

--- components/BoardComponent/index.tsx
+++ components/BoardComponent/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Pagination, Typography } from '@mui/material';
 import UploadIcon from '@mui/icons-material/Upload';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useFetchBoardInfo } from '../../queries/boards';
 import * as S from './styles';
@@ -9,18 +10,22 @@ import { ITEMS_PER_PAGE } from '../../models/consts';
 
 interface Props {
     board_id: string;
+    page: number;
 }
 
 export default function BoardComponent(props: Props) {
-    const { board_id } = props;
+    const { board_id, page } = props;
     const { data: board } = useFetchBoardInfo(board_id);
     const [openUploadDialog, setOpenUploadDialog] = useState(false);
-    const [page, setPage] = useState(1);
+    const router = useRouter();
     const { data: filesResponse, isLoading: loadingImages } = useFetchFilesInBoard(board_id, page);
     const totalPage = Math.floor((filesResponse?.totalCount || 0) / ITEMS_PER_PAGE);
 
     const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
+        const url = new URL(window.location.href);
+        url.searchParams.set('page', value.toString());
+
+        router.push(url);
     };
 
     return (
--- pages/[board_id]/index.tsx
+++ pages/[board_id]/index.tsx
@@ -1,9 +1,14 @@
 import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
 import BoardComponent from '../../components/BoardComponent';
 import MainLayout from '../../components/commons/MainLayout';
 
 export default function Board() {
     const router = useRouter();
     const { board_id } = router.query;
-    return <MainLayout>{board_id && <BoardComponent board_id={board_id as string} />}</MainLayout>;
+    const searchParams = useSearchParams();
+    const page = searchParams.get('page') || 1;
+    return <MainLayout>
+        {board_id && <BoardComponent board_id={board_id as string} page={+page} />}
+    </MainLayout>;
 }
